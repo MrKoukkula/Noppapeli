d77c9b3837971cf516cc68e2aeaef1e0dd20c8b9
Fraiyn <pe.koukkula@gmail.com>
2020-02-08 18:39:20 +0200

    Added gamescreen reveal

--- js/index-compiled.js
+++ js/index-compiled.js
@@ -1,7 +1,12 @@
 (function init() {
+  let startScreen = document.getElementById("startScreen");
+  let gameScreen = document.getElementById("gameScreen");
   let startButton = document.getElementById("startButton");
+  let playerName, noppa1, noppa2, noppa3, noppa4, noppa5;
   startButton.addEventListener("click", e => {
-    let playerInput = document.getElementById("playerName");
-    let playerName = playerInput.nodeValue;
+    playerName = document.getElementById("playerName").value;
+    console.log(playerName);
+    startScreen.style.display = "none";
+    gameScreen.style.display = "block";
   });
 })();
